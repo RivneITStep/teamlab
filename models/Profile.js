8a3878993ebda9c99f367d9428c66cea002cc8e4
valeryho <v.gorodskov1985@gmail.com>
2019-09-29 20:48:47 +0300

    1)created model profile 2/router add profile created 3/ router show profile aded(net too send user_id)return object with name+profile+gitrepositories

--- models/Profile.js
+++ models/Profile.js
@@ -2,16 +2,93 @@ const mongoose = require('mongoose');
 const ProfileSchema = new mongoose.Schema({
     user_id: {
         type: String,
-        required: true
+        require: true
     },
-    avatar: {
-        type: String,
-        required: true,
-        unique: true
+    location: {
+        type: String
     },
-    git: {
-        type: String,
+    skills: [{
+        type: [String],
         required: true
+    }],
+    githubusername: {
+        type: String
+    },
+    experience: [{
+        position: {
+            type: String,
+            
+        },
+        company: {
+            type: String,
+           
+        },
+        location: {
+            type: String
+        },
+        from: {
+            type: Date,
+            
+        },
+        to: {
+            type: Date
+        },
+        current: {
+            type: Boolean,
+            default: false
+        },
+        description: {
+            type: String
+        }
+    }],
+    education: [{
+        school: {
+            type: String,
+            
+        },
+        degree: {
+            type: String,
+            
+        },
+        fieldofstady: {
+            type: String,
+            
+        },
+        from: {
+            type: Date,
+            
+        },
+        to: {
+            type: Date
+        },
+        current: {
+            type: Boolean,
+            default: false
+        },
+        description: {
+            type: String
+        }
+    }],
+    social: {
+        youtube: {
+            type: String
+        },
+        twitter: {
+            type: String
+        },
+        facebook: {
+            type: String
+        },
+        linkedin: {
+            type: String
+        },
+        instagram: {
+            type: String
+        }
+    },
+    mainimage: {
+        type: String,
+        required: false
     },
     date: {
         type: Date,
