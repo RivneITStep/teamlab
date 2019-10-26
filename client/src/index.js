3022a734b4297069549ceb43dd8567184ea51aaf
LilyMart7 <lilymart7@gmail.com>
2019-10-26 21:15:58 +0300

    creating SPA Router

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route } from 'react-router'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './index.scss';
 import Home from './components/Home/home';
 import About from './components/About/about';
