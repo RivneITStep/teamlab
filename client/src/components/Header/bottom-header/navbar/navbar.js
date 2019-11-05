26813f87d7b90a3ede28ef5b5f1e0640dfa7a707
LilyMart7 <lilymart7@gmail.com>
2019-11-05 22:33:34 +0200

    added Font Awesome to the project

--- client/src/components/Header/bottom-header/navbar/navbar.js
+++ client/src/components/Header/bottom-header/navbar/navbar.js
@@ -15,36 +15,56 @@ const NavBar = () => {
             <div>
               <div>
                 <ul>
-                  <li>
-                    <Link to="/">Home</Link>
-                  </li>
-                  <li>
-                    <Link to="about">About</Link>
-                  </li>
-                  <li>
-                    <Link to="projects">Projects</Link>
-                  </li>
-                  <li>
-                    <Link to="news">News</Link>
-                  </li>
-                  <li>
-                    <Link to="faq">FAQ</Link>
-                  </li>
-                  <li>
-                    <Link to="posts">Posts</Link>
-                  </li>
-                  <li>
-                    <Link to="contact">Contact</Link>
-                  </li>
-                  <li>
-                    <Link to="login">Login</Link>
-                  </li>
-                  <li>
-                    <Link to="register">Register</Link>
-                  </li>
-                  <li>
-                    <Link to="profile">Profile</Link>
-                  </li>
+                <li className="item">
+              <Link to="/" className="link-nav">
+                Home
+              </Link>
+            </li>
+            <li className="item">
+              <Link to="about" className="link-nav">
+                About
+              </Link>
+            </li>
+            <li className="item">
+              <Link to="projects" className="link-nav">
+                Projects
+              </Link>
+            </li>
+            <li className="item">
+              <Link to="news" className="link-nav">
+                News
+              </Link>
+            </li>
+            <li className="item">
+              <Link to="faq" className="link-nav">
+                FAQ
+              </Link>
+            </li>
+            <li className="item">
+              <Link to="posts" className="link-nav">
+                Posts
+              </Link>
+            </li>
+            <li className="item">
+              <Link to="contact" className="link-nav">
+                Contact
+              </Link>
+            </li>
+            <li className="item">
+              <Link to="login" className="link-nav">
+                Login
+              </Link>
+            </li>
+            <li className="item">
+              <Link to="register" className="link-nav">
+                Register
+              </Link>
+            </li>
+            <li className="item">
+              <Link to="profile" className="link-nav">
+                Profile
+              </Link>
+            </li>
                 </ul>
               </div>
             </div>
