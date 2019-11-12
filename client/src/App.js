846cc749035c0e8984c0b3c990235f84b4200729
pancheliuga <pan4ik@gmail.com>
2019-11-13 00:52:53 +0200

    Create Login Component + some refactoring Register Component

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Home from "./components/Home/home";
@@ -18,28 +18,40 @@ import "./App.scss";
 
 import { Provider } from "react-redux";
 import store from "./store";
+import { loadUser } from "./actions/auth";
+import setAuthToken from "./utils/setAuthToken";
 
-const App = () => (
-  <Provider store={store}>
-    <Router>
-      <Fragment>
-        <Header />
-        <Route path="/" exact component={Home} />
-        <Route path="/about" exact component={About} />
-        <Route path="/projects" exact component={Projects} />
-        <Route path="/news" exact component={News} />
-        <Route path="/faq" exact component={Faq} />
-        <Route path="/posts" exact component={Posts} />
-        <Route path="/contact" exact component={Contact} />
-        <Switch>
-          <Route path="/login" exact component={Login} />
-          <Route path="/register" exact component={Register} />
-        </Switch>
-        <Route path="/profile" exact component={Profile} />
-        <Footer />
-      </Fragment>
-    </Router>
-  </Provider>
-);
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Header />
+          <Route path="/" exact component={Home} />
+          <Route path="/about" exact component={About} />
+          <Route path="/projects" exact component={Projects} />
+          <Route path="/news" exact component={News} />
+          <Route path="/faq" exact component={Faq} />
+          <Route path="/posts" exact component={Posts} />
+          <Route path="/contact" exact component={Contact} />
+          <Switch>
+            <Route path="/login" exact component={Login} />
+            <Route path="/register" exact component={Register} />
+          </Switch>
+          <Route path="/profile" exact component={Profile} />
+          <Footer />
+        </Fragment>
+      </Router>
+    </Provider>
+  );
+};
 
 export default App;
--- client/src/components/Header/top-heder/buttons-header/buttons-header.js
+++ client/src/components/Header/top-heder/buttons-header/buttons-header.js
@@ -1,30 +1,67 @@
 import React, { Fragment } from "react";
-import "./buttons-header.scss";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { logout } from "../../../../actions/auth";
 
-const ButtonsHeader = () => {
+import "./buttons-header.scss";
+
+const ButtonsHeader = ({ auth: { isAuthenticated, loading }, logout }) => {
+  const authLinks = (
+    <Link to="/">
+      <button
+        onClick={logout}
+        type="button"
+        className="btn btn-login-header btn-outline-primary"
+      >
+        Logout
+      </button>
+    </Link>
+  );
+
+  const guestLinks = (
+    <Fragment>
+      <Link to="/login">
+        <button
+          type="button"
+          className="btn btn-login-header btn-outline-primary"
+        >
+          Login
+        </button>
+      </Link>
+
+      <Link to="/register">
+        <button
+          type="button"
+          className="btn btn-register-header btn-outline-primary"
+        >
+          Register
+        </button>
+      </Link>
+    </Fragment>
+  );
+
   return (
     <Fragment>
       <div className="header-top-right col">
-        <Link to="/login">
-          <button
-            type="button"
-            className="btn btn-login-header btn-outline-primary"
-          >
-            Login
-          </button>
-        </Link>
-        <Link to="/register">
-          <button
-            type="button"
-            className="btn btn-register-header btn-outline-primary"
-          >
-            Register
-          </button>
-        </Link>
+        {!loading && (
+          <Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>
+        )}
       </div>
     </Fragment>
   );
 };
 
-export default ButtonsHeader;
+ButtonsHeader.propTypes = {
+  logout: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(
+  mapStateToProps,
+  { logout }
+)(ButtonsHeader);
