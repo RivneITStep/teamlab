084550417b926fc62eeabbcf6c1abcefb47a1988
LilyMart7 <lilymart7@gmail.com>
2019-11-14 19:21:34 +0200

    added Single page, Single post and Forgot password pages

---
+++ client/src/components/Single-post/single-post.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+import "./single-post.scss";
+
+const SinglePost = () => {
+  return (
+    <Fragment>
+      <div>Single Post</div>
+    </Fragment>
+  );
+};
+
+export default SinglePost;
