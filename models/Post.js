ec1187bbe00554eb39d8c18821b304f8c159f05e
carryCh <romkacarry@gmail.com>
2019-11-26 23:31:49 +0200

    Huge refactoring of back end: 1) Added likes model; 2) Added views for single posts; 3) Added functionality(like-controller): add like for post, delete like from post; 4) Now we can see number of comments for each post; 5) Refactored comment-controller.js and post-controller.js

--- models/Post.js
+++ models/Post.js
@@ -1,24 +1,40 @@
 const mongoose = require("mongoose");
-const { Schema } = mongoose;
+const {Schema} = mongoose;
 
 const PostSchema = new Schema({
-  author: {
-    id: { type: Schema.Types.ObjectId, ref: "User", required: true },
-    name: { type: String }
-  },
-  title: {
-    type: String,
-    required: true
-  },
-  text: {
-    type: String,
-    required: true
-  },
-  date: {
-    type: Date,
-    default: Date.now,
-    required: true
-  }
+    author: {
+        id: {type: Schema.Types.ObjectId, ref: "User", required: true},
+        name: {type: String}
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    comments: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Comment'
+        }
+    ],
+    likes: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Like'
+        }
+    ],
+    views: {
+        type: Number,
+        default: 0
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+        required: true
+    }
 });
 
 module.exports = Post = mongoose.model("Post", PostSchema);
