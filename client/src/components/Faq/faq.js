67ad3213fcb52de44c4aff8f088c772fe9461da3
LilyMart7 <lilymart7@gmail.com>
2019-12-06 00:55:30 +0200

    Added FAQ Component

--- client/src/components/Faq/faq.js
+++ client/src/components/Faq/faq.js
@@ -4,54 +4,54 @@ import "./faq.scss";
 const Faq = () => {
   return (
     <Fragment>
-      <div class="container- fluid faq-top-bg">
-                <div class="container faq-body">
+      <div className="container- fluid faq-top-bg">
+                <div className="container faq-body">
                   <section>
-                    <div class="row faq-top-header">
-                      <h1 class="faq-text-header">FAQ</h1>
+                    <div className="row faq-top-header">
+                      <h1 className="faq-text-header">FAQ</h1>
                     </div>
                   </section>
                 </div>
               </div>
 
-    <div class="container-fluid">
+    <div className="container-fluid">
 
-        <div class="container faq-body">
+        <div className="container faq-body">
 
-                <div class="row ">
-                        <div class="col works-text">Frequently Asked Questions</div>
+                <div className="row ">
+                        <div className="col works-text">Frequently Asked Questions</div>
                     </div>
 
-            <div class="row faq-blocks">
-                <div class="col faq-top">1. What is TeamLab?</div>
+            <div className="row faq-blocks">
+                <div className="col faq-top">1. What is TeamLab?</div>
 
-                <div class="col faq-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque tempore soluta quidem aut earum voluptatem, praesentium ratione voluptas non aliquid natus quisquam dolorum officia expedita accusamus sequi nostrum! Officiis, id!
+                <div className="col faq-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque tempore soluta quidem aut earum voluptatem, praesentium ratione voluptas non aliquid natus quisquam dolorum officia expedita accusamus sequi nostrum! Officiis, id!
                         Dolorem nemo praesentium labore nulla aut explicabo quas, officiis eveniet sequi repudiandae doloremque debitis alias reiciendis neque aperiam maiores rem dolores amet unde placeat fugiat nisi. Inventore voluptatibus consequatur velit?
                         Aliquam, animi? Illum aut dolorum libero, odio sapiente nesciunt. Facilis doloribus, hic nam repellendus incidunt eligendi commodi cupiditate ipsam cumque voluptates veritatis dolor quam neque, recusandae natus. Impedit, officia aperiam?
                         Reprehenderit magni deleniti ex incidunt repellendus rem sequi dolorem. Laboriosam rerum quaerat laudantium minima. Velit id magnam quod iure consequatur quo debitis animi non atque? Nesciunt vitae inventore quibusdam explicabo.
                         Expedita, cumque totam iure consequuntur ea perspiciatis numquam distinctio quas, ipsum error dolorum commodi, provident atque odio rerum neque iste quae! Id, consequuntur cum repellat aut quos rem maiores officia.</div>
                     </div>
 
-            <div class="row faq-blocks">
-                <div class="col faq-top">2. How can I come up with an idea?</div>
+            <div className="row faq-blocks">
+                <div className="col faq-top">2. How can I come up with an idea?</div>
 
-                <div class="col faq-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque tempore soluta quidem aut earum voluptatem, praesentium ratione voluptas non aliquid natus quisquam dolorum officia expedita accusamus sequi nostrum! Officiis, id!
+                <div className="col faq-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque tempore soluta quidem aut earum voluptatem, praesentium ratione voluptas non aliquid natus quisquam dolorum officia expedita accusamus sequi nostrum! Officiis, id!
                         Dolorem nemo praesentium labore nulla aut explicabo quas, officiis eveniet sequi repudiandae doloremque debitis alias reiciendis neque aperiam maiores rem dolores amet unde placeat fugiat nisi. Inventore voluptatibus consequatur velit?
                         Aliquam, animi? Illum aut dolorum libero, odio sapiente nesciunt. Facilis doloribus, hic nam repellendus incidunt eligendi commodi cupiditate ipsam cumque voluptates veritatis dolor quam neque, recusandae natus. Impedit, officia aperiam?
                         Reprehenderit magni deleniti ex incidunt repellendus rem sequi dolorem. Laboriosam rerum quaerat laudantium minima. Velit id magnam quod iure consequatur quo debitis animi non atque? Nesciunt vitae inventore quibusdam explicabo.
                         Expedita, cumque totam iure consequuntur ea perspiciatis numquam distinctio quas, ipsum error dolorum commodi, provident atque odio rerum neque iste quae! Id, consequuntur cum repellat aut quos rem maiores officia.</div>
                     </div>
 
-            <div class="row faq-blocks">
-                <div class="col faq-top">3. Who can participate in the project?</div>
+            <div className="row faq-blocks">
+                <div className="col faq-top">3. Who can participate in the project?</div>
 
-                <div class="col faq-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit praesentium beatae quia dignissimos iste dolorum eos alias neque vitae nesciunt officia, veniam, natus tenetur molestiae. Nostrum cumque accusantium similique voluptates!</div>
+                <div className="col faq-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit praesentium beatae quia dignissimos iste dolorum eos alias neque vitae nesciunt officia, veniam, natus tenetur molestiae. Nostrum cumque accusantium similique voluptates!</div>
                     </div>
 
-            <div class="row faq-blocks">
-                <div class="col faq-top">4. Where can I find already implemented projects?</div>
+            <div className="row faq-blocks">
+                <div className="col faq-top">4. Where can I find already implemented projects?</div>
 
-                <div class="col faq-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque tempore soluta quidem aut earum voluptatem, praesentium ratione voluptas non aliquid natus quisquam dolorum officia expedita accusamus sequi nostrum! Officiis, id!
+                <div className="col faq-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque tempore soluta quidem aut earum voluptatem, praesentium ratione voluptas non aliquid natus quisquam dolorum officia expedita accusamus sequi nostrum! Officiis, id!
                         Dolorem nemo praesentium labore nulla aut explicabo quas, officiis eveniet sequi repudiandae doloremque debitis alias reiciendis neque aperiam maiores rem dolonimi non atque? Nesciunt vitae inventore quibusdam explicabo.
                         Expedita, cumque totam iure consequuntur ea perspiciatis numquam distinctio quas, ipsum error dolorum commodi, provident atque odio rerum neque iste quae! Id, consequuntur cum repellat aut quos rem maiores officia.</div>
                     </div>
