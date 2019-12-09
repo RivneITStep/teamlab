512232534e14e8d2479a448df02b197302942a2a
LilyMart7 <lilymart7@gmail.com>
2019-12-09 22:46:25 +0200

    Updated News Component

--- client/src/components/News/news.js
+++ client/src/components/News/news.js
@@ -4,7 +4,199 @@ import "./news.scss";
 const News = () => {
   return (
     <Fragment>
-      <div>News</div>
+      <div className="container- fluid news-top-bg">
+        <div className="container news-body">
+          <section>
+            <div className="row news-top">
+              <h1 className="news-text">News</h1>
+            </div>
+          </section>
+        </div>
+      </div>
+
+      <div class="container-fluid">
+        <div class="container news-body">
+          <div class="row">
+            <section class="news-block col-md-4">
+              <div class="row news-block-body ">
+                <div class="col-md-12">
+                  <div class="news-img"></div>
+                </div>
+
+                <div class="news-details">
+                  <h5>Title</h5>
+                  <p>25th February, 2018</p>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Natus temporibus consequuntur optio voluptatibus officia
+                    placeat fuga, blanditiis porro similique animi repellendus,
+                    aperiam est distinctio labore, facere delectus ip- sam nisi
+                    in. At, harum quaerat. Dolor?
+                  </p>
+                  <h6>by Bill Gates</h6>
+
+                  <div class="news-more-btn ">
+                    <button
+                      type="button"
+                      class="btn btn-outline-dark more-news"
+                    >
+                      Read more
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </section>
+            <section class="news-block col-md-4">
+              <div class="row news-block-body ">
+                <div class="col-md-12">
+                  <div class="news-img"></div>
+                </div>
+
+                <div class="news-details">
+                  <h5>Title</h5>
+                  <p>25th February, 2018</p>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Natus temporibus consequuntur optio voluptatibus officia
+                    placeat fuga, blanditiis porro similique animi repellendus,
+                    aperiam est distinctio labore, facere delectus ip- sam nisi
+                    in. At, harum quaerat. Dolor?
+                  </p>
+                  <h6>by Bill Gates</h6>
+
+                  <div class="news-more-btn ">
+                    <button
+                      type="button"
+                      class="btn btn-outline-dark more-news"
+                    >
+                      Read more
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </section>
+            <section class="news-block col-md-4">
+              <div class="row news-block-body ">
+                <div class="col-md-12">
+                  <div class="news-img"></div>
+                </div>
+
+                <div class="news-details">
+                  <h5>Title</h5>
+                  <p>25th February, 2018</p>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Natus temporibus consequuntur optio voluptatibus officia
+                    placeat fuga, blanditiis porro similique animi repellendus,
+                    aperiam est distinctio labore, facere delectus ip- sam nisi
+                    in. At, harum quaerat. Dolor?
+                  </p>
+                  <h6>by Bill Gates</h6>
+
+                  <div class="news-more-btn ">
+                    <button
+                      type="button"
+                      class="btn btn-outline-dark more-news"
+                    >
+                      Read more
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </section>
+          </div>
+
+          <div class="row">
+            <section class="news-block col-md-4">
+              <div class="row news-block-body ">
+                <div class="col-md-12">
+                  <div class="news-img"></div>
+                </div>
+
+                <div class="news-details">
+                  <h5>Title</h5>
+                  <p>25th February, 2018</p>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Natus temporibus consequuntur optio voluptatibus officia
+                    placeat fuga, blanditiis porro similique animi repellendus,
+                    aperiam est distinctio labore, facere delectus ip- sam nisi
+                    in. At, harum quaerat. Dolor?
+                  </p>
+                  <h6>by Bill Gates</h6>
+
+                  <div class="news-more-btn ">
+                    <button
+                      type="button"
+                      class="btn btn-outline-dark more-news"
+                    >
+                      Read more
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </section>
+            <section class="news-block col-md-4">
+              <div class="row news-block-body ">
+                <div class="col-md-12">
+                  <div class="news-img"></div>
+                </div>
+
+                <div class="news-details">
+                  <h5>Title</h5>
+                  <p>25th February, 2018</p>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Natus temporibus consequuntur optio voluptatibus officia
+                    placeat fuga, blanditiis porro similique animi repellendus,
+                    aperiam est distinctio labore, facere delectus ip- sam nisi
+                    in. At, harum quaerat. Dolor?
+                  </p>
+                  <h6>by Bill Gates</h6>
+
+                  <div class="news-more-btn ">
+                    <button
+                      type="button"
+                      class="btn btn-outline-dark more-news"
+                    >
+                      Read more
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </section>
+            <section class="news-block col-md-4">
+              <div class="row news-block-body ">
+                <div class="col-md-12">
+                  <div class="news-img"></div>
+                </div>
+
+                <div class="news-details">
+                  <h5>Title</h5>
+                  <p>25th February, 2018</p>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Natus temporibus consequuntur optio voluptatibus officia
+                    placeat fuga, blanditiis porro similique animi repellendus,
+                    aperiam est distinctio labore, facere delectus ip- sam nisi
+                    in. At, harum quaerat. Dolor?
+                  </p>
+                  <h6>by Bill Gates</h6>
+
+                  <div class="news-more-btn ">
+                    <button
+                      type="button"
+                      class="btn btn-outline-dark more-news"
+                    >
+                      Read more
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </section>
+          </div>
+        </div>
+      </div>
     </Fragment>
   );
 };
