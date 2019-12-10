da0303272e2810c2389e5ecbaa7d0f95b130009a
LilyMart7 <lilymart7@gmail.com>
2019-12-10 18:54:35 +0200

    Added content @  About us Component

--- client/src/components/About/about.js
+++ client/src/components/About/about.js
@@ -4,7 +4,15 @@ import "./about.scss";
 const About = () => {
   return (
     <Fragment>
-      <div>About</div>
+  <div className="container- fluid about-top-bg">
+        <div className="container about-top-body">
+          <section>
+            <div className="row about-top">
+              <h1 className="about-top-text">About us</h1>
+            </div>
+          </section>
+        </div>
+      </div>
     </Fragment>
   );
 };
