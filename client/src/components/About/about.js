e7a1cf5e014b91c53329c5b5da8dbfc6fdead2cb
LilyMart7 <lilymart7@gmail.com>
2019-12-10 20:53:23 +0200

    UI refactoring About and FAQ Components

--- client/src/components/About/about.js
+++ client/src/components/About/about.js
@@ -4,7 +4,7 @@ import "./about.scss";
 const About = () => {
   return (
     <Fragment>
-  <div className="container- fluid about-top-bg">
+      <div className="container- fluid about-top-bg">
         <div className="container about-top-body">
           <section>
             <div className="row about-top">
@@ -13,6 +13,64 @@ const About = () => {
           </section>
         </div>
       </div>
+
+  
+        <div className="container">
+          <div className="row blocks_top">
+            <div className="col single_top">
+              <div className="title_top">
+                <h4>My First Text</h4>
+              </div>
+              <div className="desc_top">
+                <p>
+                  Usage of the Internet is becoming more common due to rapid
+                  advancement of technology.
+                </p>
+                <button
+                  type="button"
+                  className="btn btn-outline-dark more-news"
+                >
+                  Join now
+                </button>
+              </div>
+            </div>
+            <div className="col single_top">
+              <div className="title_top">
+                <h4>My First Text</h4>
+              </div>
+              <div className="desc_top">
+                <p>
+                  Usage of the Internet is becoming more common due to rapid
+                  advancement of technology.
+                </p>
+                <button
+                  type="button"
+                  className="btn btn-outline-dark more-news"
+                >
+                  Join now
+                </button>
+              </div>
+            </div>
+            <div className="col single_top">
+              <div className="title_top">
+                <h4>My First Text</h4>
+              </div>
+              <div className="desc_top">
+                <p>
+                  Usage of the Internet is becoming more common due to rapid
+                  advancement of technology.
+                </p>
+                <button
+                  type="button"
+                  className="btn btn-outline-dark more-news"
+                >
+                  Join now
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+ 
     </Fragment>
   );
 };
