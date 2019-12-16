da4170315a356ecff2423e350324a65eaf164a36
Andrii Trofimchuk <trofimchuk.an@gmail.com>
2019-12-16 17:16:51 +0200

    Refactor URL

--- client/src/services/teamlabstore-service.js
+++ client/src/services/teamlabstore-service.js
@@ -1,65 +1,46 @@
 import axios from "axios";
-
 export default class TeamlabstoreService {
-  //PROJECT URL
-  url = "http://13.53.138.219:5000";
   //CUSTOMIZE AXIOS
   config = {
     headers: {
       "Content-Type": "application/json"
     }
   };
-
   //POSTS SERVICE (CRUD)
   getPosts() {
-    return axios.get(`${this.url}/api/posts`);
+    return axios.get(`/api/posts`);
   }
-
   getSinglePost(id) {
-    return axios.get(`${this.url}/api/posts/single-post/${id}`);
+    return axios.get(`/api/posts/single-post/${id}`);
   }
-
   deleteSinglePost(id) {
-    return axios.delete(`${this.url}/api/posts/single-post/${id}/delete`);
+    return axios.delete(`/api/posts/single-post/${id}/delete`);
   }
-
   addNewPost() {
-    return axios.post(`${this.url}/api/posts/add-new-post`);
+    return axios.post(`/api/posts/add-new-post`);
   }
-
   updateSinglePost(id, body) {
-    return axios.put(
-      `${this.url}/api/posts/single-post/${id}/update`,
-      body,
-      this.config
-    );
+    return axios.put(`/api/posts/single-post/${id}/update`, body, this.config);
   }
-
   setLikeToPost(postId) {
-    return axios.post(`${this.url}/api/posts/like/${postId}`);
+    return axios.post(`/api/posts/like/${postId}`);
   }
-
   deleteLikeFromPost(postId) {
-    return axios.post(`${this.url}/api/posts/like/delete/${postId}`);
+    return axios.post(`/api/posts/like/delete/${postId}`);
   }
-
   addCommentToSinglePost(id, body) {
     return axios.post(
-      `${this.url}/api/posts/single-post/${id}/add-comment`,
+      `/api/posts/single-post/${id}/add-comment`,
       body,
       this.config
     );
   }
-
   deleteCommentToSinglePost(id, commentId) {
     return axios.delete(
-      `${this.url}/api/posts/single-post/${id}/delete-comment/${commentId}`
+      `/api/posts/single-post/${id}/delete-comment/${commentId}`
     );
   }
-
   updateComment(commentId) {
-    return axios.delete(
-      `${this.url}/api/posts/single-post/update-comment/${commentId}`
-    );
+    return axios.delete(`/api/posts/single-post/update-comment/${commentId}`);
   }
 }
